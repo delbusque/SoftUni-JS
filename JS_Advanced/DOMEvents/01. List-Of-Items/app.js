e64a910974937e3ead9d180add2432738bd90bf1
delbusque <del.busque@gmail.com>
2021-06-23 08:43:55 +0300

    Added DOM Events

---
+++ JS_Advanced/DOMEvents/01. List-Of-Items/app.js
@@ -0,0 +1,10 @@
+function addItem() {
+    let inputTextElement = document.getElementById('newItemText');
+    let listItemsElement = document.getElementById('items');
+
+    const newLi = document.createElement('li');
+    listItemsElement.appendChild(newLi);
+
+    newLi.textContent = inputTextElement.value;
+    inputTextElement.value = '';
+}
