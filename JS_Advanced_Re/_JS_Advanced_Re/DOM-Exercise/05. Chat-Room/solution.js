dcb7ab5d18b6e9fd8d9f2f9564ebc66fcf9d1f91
Ventsislav Peychev <del.busque@gmail.com>
2022-01-18 12:29:42 +0200

    Added Chat Room

--- JS_Advanced_Re/_JS_Advanced_Re/DOM-Exercise/05. Chat-Room/solution.js
+++ JS_Advanced_Re/_JS_Advanced_Re/DOM-Exercise/05. Chat-Room/solution.js
@@ -1,5 +1,16 @@
 function solve() {
-   //TODO...
+    let messagesElement = document.getElementById('chat_messages');
+    let sendButtonElement = document.getElementById('send');
+
+    sendButtonElement.addEventListener('click', (e) => {
+        let chatInputElement = document.getElementById('chat_input');
+
+
+        let newMessage = document.createElement('div');
+        newMessage.setAttribute('class', 'message my-message');
+        newMessage.textContent = chatInputElement.value;
+        messagesElement.appendChild(newMessage);
+
+        chatInputElement.value = '';
+    })
 }
-
-
