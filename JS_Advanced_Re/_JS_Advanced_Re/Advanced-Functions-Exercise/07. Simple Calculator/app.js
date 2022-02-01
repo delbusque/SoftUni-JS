137296ecfccc45cfacb2a0d785182f317e15c58f
delbusque <del.busque@gmail.com>
2022-02-01 10:39:41 +0200

    Update app.js

--- JS_Advanced_Re/_JS_Advanced_Re/Advanced-Functions-Exercise/07. Simple Calculator/app.js
+++ JS_Advanced_Re/_JS_Advanced_Re/Advanced-Functions-Exercise/07. Simple Calculator/app.js
@@ -1,7 +1,28 @@
 function calculator() {
-    // TODO:
+    let numbOneElement;
+    let numbTwoElement;
+    let resultElement;
+
+    let action = {
+        init(selector1, selector2, resultSelector) {
+            numbOneElement = document.querySelector(selector1);
+            numbTwoElement = document.querySelector(selector2);
+            resultElement = document.querySelector(resultSelector);
+        },
+        add() {
+            resultElement.value = Number(numbOneElement.value) + Number(numbTwoElement.value);
+            numbOneElement.value = '';
+            numbTwoElement.value = '';
+        },
+        subtract() {
+            resultElement.value = Number(numbOneElement.value) - Number(numbTwoElement.value);
+            numbOneElement.value = '';
+            numbTwoElement.value = '';
+        }
+    }
+
+    return action;
 }
 
-
-
-
+const calculate = calculator();
+calculate.init('#num1', '#num2', '#result'); 
