3c90b8a948e4aaf41abca2e320c1a9dd860c1574
delbusque <del.busque@gmail.com>
2022-02-01 11:08:23 +0200

    Update script.js

--- JS_Advanced_Re/_JS_Advanced_Re/Advanced-Functions-Exercise/08. Next Article/script.js
+++ JS_Advanced_Re/_JS_Advanced_Re/Advanced-Functions-Exercise/08. Next Article/script.js
@@ -1,3 +1,13 @@
 function getArticleGenerator(articles) {
-    // TODO
+    let contentElement = document.querySelector('#content');
+
+    function action() {
+        if (articles.length > 0) {
+            let articleElement = document.createElement('article');
+            articleElement.textContent = articles.splice(0, 1);
+            contentElement.appendChild(articleElement);
+        }
+    }
+
+    return action;
 }
