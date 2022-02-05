a142f35b21bb291a8a4829899700944c796aec32
delbusque <del.busque@gmail.com>
2022-02-05 13:12:30 +0200

    Create townsToJSON.js

---
+++ JS_Advanced_Re/Objects-Classes/townsToJSON.js
@@ -0,0 +1,10 @@
+function solve(input) {
+    let headings = input[0].split(' | ');
+    console.log(headings);
+
+}
+
+solve(['| Town | Latitude | Longitude |',
+    '| Sofia | 42.696552 | 23.32601 |',
+    '| Beijing | 39.913818 | 116.363625 |'
+])
