588dc059dc4aad08d05497082603154580e301b4
Ventsislav Peychev <del.busque@gmail.com>
2022-02-27 15:09:13 +0200

    Create juiceFlavours.js

---
+++ JS_Advanced_Re/_JS_Advanced_Re/Classes-Exercise/juiceFlavours.js
@@ -0,0 +1,18 @@
+function solve(arr) {
+    let input = [];
+    arr.forEach(x => {
+        input.push(x.split(' => '))
+    });
+
+    let flavours = new Map();
+
+
+
+}
+
+solve(['Orange => 2000',
+    'Peach => 1432',
+    'Banana => 450',
+    'Peach => 600',
+    'Strawberry => 549'
+])
