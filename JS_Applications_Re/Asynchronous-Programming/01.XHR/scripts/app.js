47021086595b979df0d39e0e296abf8a8e61028f
delbusque <del.busque@gmail.com>
2022-03-07 12:11:51 +0200

    Added Load Repos

--- JS_Applications_Re/Asynchronous-Programming/01.XHR/scripts/app.js
+++ JS_Applications_Re/Asynchronous-Programming/01.XHR/scripts/app.js
@@ -4,10 +4,10 @@ function loadRepos() {
     let divDocument = document.getElementById('res');
 
     fetch(`${url}/people/1`).then(res => res.json()).then(data => {
-        divDocument.textContent = JSON.stringify(data.name);
+        let lukeData = Object.create(data);
+        fetch(`${lukeData.homeworld}`).then(res => res.json()).then(data => {
+            let lukePlanet = Object.create(data);
+            divDocument.textContent = `This is the greatest jedy ${lukeData.name} and his homeworld is ${lukePlanet.name} ...`;
+        })
     });
-
-
-
-
 }
--- JS_Applications_Re/Asynchronous-Programming/02.Github-Repos/scripts/app.js
+++ JS_Applications_Re/Asynchronous-Programming/02.Github-Repos/scripts/app.js
@@ -1,3 +1,29 @@
 function loadRepos() {
-	console.log("TODO...");
+    const reposDisplay = document.getElementById('repos');
+    reposDisplay.innerHTML = '';
+    const usernameDisplay = document.getElementById('username');
+
+    let url = `https://api.github.com/users/${usernameDisplay.value}/repos`;
+
+    let errData = {};
+
+    fetch(url)
+        .then(res => {
+            return res.json()
+        })
+        .then(data => {
+            errData.message = data.message;
+            data.forEach(repo => {
+                let newLiElement = document.createElement('li');
+                let newAnchorElement = document.createElement('a');
+                newAnchorElement.setAttribute('href', repo.html_url);
+                newAnchorElement.textContent = repo.full_name;
+
+                newLiElement.appendChild(newAnchorElement);
+                reposDisplay.appendChild(newLiElement);
+            });
+        })
+        .catch(() => {
+            reposDisplay.textContent = errData.message;
+        })
 }
