95977e2ea6ad10cde60e41f077027f348a417a8f
Ventsislav Peychev <del.busque@gmail.com>
2022-05-02 13:35:12 +0300

    Renamed directories

---
+++ JS-Web/intro-to-nodejs/index.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const fs = require('fs');
+
+let app = http.createServer((req, res) => {
+
+    switch (req.url) {
+        case '/':
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            })
+            res.write('<h2>Home page</h2> <a href="/cats">Cats</a>')
+            res.end();
+            break;
+
+        case '/cats':
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            })
+            let result = fs.readFileSync('./views/cats.html');
+            res.write(result)
+            res.end();
+            break;
+
+        default:
+            res.write('node.js says Error 404');
+            res.end();
+            break;
+    }
+})
+
+app.listen(3000);
+console.log('Node.js is running on port 3000...');
