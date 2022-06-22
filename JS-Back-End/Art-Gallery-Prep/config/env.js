11c426c28992b8250a9349ece4ddfa9ad2afc980
delbusque <del.busque@gmail.com>
2022-06-22 13:36:45 +0300

    Added authentication, jwt, cookie session

--- JS-Back-End/Art-Gallery-Prep/config/env.js
+++ JS-Back-End/Art-Gallery-Prep/config/env.js
@@ -1,2 +1,4 @@
 exports.PORT = 3000;
 exports.DB_QUERYSTRING = 'mongodb://localhost:27017/art-gallery';
+exports.SALT_ROUNDS = 10;
+exports.SECRET = 'a237d47f50d2f64f1bae7e1f313e247d';
--- JS-Back-End/Art-Gallery-Prep/controllers/authController.js
+++ JS-Back-End/Art-Gallery-Prep/controllers/authController.js
@@ -1,15 +1,49 @@
 const router = require('express').Router();
+const authService = require('../services/authService.js');
+const { COOKIE_SESSION_NAME } = require('../constants.js');
 
 router.get('/login', (req, res) => {
     res.render('auth/login');
 });
 
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    const user = await authService.login(username, password);
+    const token = await authService.createToken(user);
+
+    res.cookie(COOKIE_SESSION_NAME, token);
+    res.redirect('/');
+
+})
+
 router.get('/register', (req, res) => {
     res.render('auth/register');
 });
 
-router.post('/register', (req, res) => {
-    console.log(req.body);
-    res.end();
+router.post('/register', async (req, res) => {
+    const { username, password, repeatPassword, address } = req.body;
+
+    if (password !== repeatPassword) {
+        return res.render('auth/register', { error: 'Password missmatch !' })
+    }
+
+    try {
+        const createdUser = await authService.create({ username, password, address });
+        const token = await authService.createToken(createdUser);
+
+        res.cookie(COOKIE_SESSION_NAME, token);
+        res.redirect('/');
+
+    } catch (error) {
+        // Add mongoose error mapper
+        return res.render('auth/register', { error: 'db error !' })
+    }
+
+});
+
+router.get('/logout', (req, res) => {
+    res.clearCookie(COOKIE_SESSION_NAME);
+    res.redirect('/');
 })
 module.exports = router;
