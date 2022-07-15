ace2e5c0af57ef30f2f5ab3ba1ade473c27e1c36
Ventsislav Peychev <del.busque@gmail.com>
2022-07-15 13:19:00 +0300

    Added User Details view

--- React-JS/Workshop-Components/user-list/client/src/App.js
+++ React-JS/Workshop-Components/user-list/client/src/App.js
@@ -1,15 +1,36 @@
-import './App.css';
+import { useState, useEffect } from 'react';
+
+import * as userService from './services/userService';
+
 import { Footer } from './components/common/Footer';
 import { Header } from './components/common/Header';
+import { Search } from './components/main/search/Search';
 import { Section } from './components/main/section/Section';
+import { UserList } from './components/main/user-list/UserList';
+import './App.css';
+
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    userService.getAll().then(users => setUsers(users))
+  }, [])
+
+
   return (
     <div>
       <Header />
 
       <main class="main">
-        <Section />
+        <section class="card users-container">
+
+          <Search />
+          <UserList users={users} />
+
+
+          <Section />
+        </section>
       </main>
 
       <Footer />
