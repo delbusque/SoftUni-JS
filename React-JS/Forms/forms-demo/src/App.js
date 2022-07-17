d9c4a18dfb8eae1b2b8b5eafa71c366fd57e86af
Ventsislav Peychev <del.busque@gmail.com>
2022-07-17 12:06:54 +0300

    Added Forms demo

---
+++ React-JS/Forms/forms-demo/src/App.js
@@ -0,0 +1,86 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+
+  const [values, setValues] = useState({
+    username: '',
+    password: '',
+    gender: 'f',
+    age: '',
+    userType: '',
+    tac: false,
+    bio: '',
+
+  })
+
+  const onChangeHandler = (e) => {
+    setValues(oldValues => ({
+      ...oldValues,
+      [e.target.name]: e.target.type === 'checkbox' ? e.target.checked : e.target.value
+    }))
+  }
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log(values);
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <span><h4>React Forms</h4></span>
+
+        <form >
+          <div>
+            <label htmlFor="username">username: </label>
+            <input type="text" id="username" name='username' value={values.username} onChange={onChangeHandler} />
+          </div>
+
+          <div>
+            <label htmlFor="password">password: </label>
+            <input type="password" id="password" name='password' value={values.password} onChange={onChangeHandler} />
+          </div>
+
+          <div>
+            <label htmlFor="gender">gender: </label>
+            <select name="gender" id="gender" onChange={onChangeHandler}>
+              <option value="f">female</option>
+              <option value="m">male</option>
+            </select>
+          </div>
+
+          <div>
+            <label htmlFor="age">age: </label>
+            <input type="number" id='age' name='age' value={values.age} onChange={onChangeHandler} />
+          </div>
+
+          <div>
+            <label htmlFor="bio">bio: </label>
+            <textarea name="bio" id="bio" cols="30" rows="10" value={values.bio} onChange={onChangeHandler} />
+          </div>
+
+          <div>
+            <label htmlFor="individual-user">individual:</label>
+            <input type="radio" id='individual-user' name='userType' defaultValue='individual' onChange={onChangeHandler} />
+            <label htmlFor="corporate-user">  corporate:</label>
+            <input type="radio" id='corporate-user' name='userType' value='corporate' onChange={onChangeHandler} />
+          </div>
+
+          <div>
+            <label htmlFor="tac">TaC: </label>
+            <input type="checkbox" id='tac' name='tac' checked={values.tac} onChange={onChangeHandler} />
+          </div>
+
+
+          <div><button onClick={onSubmitHandler}>Send</button></div>
+
+        </form>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
