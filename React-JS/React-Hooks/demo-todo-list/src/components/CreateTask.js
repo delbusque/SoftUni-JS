81309c03f443a0242e3738b335d056a4e6592291
delbusque <del.busque@gmail.com>
2022-08-02 08:59:09 +0300

    Update CreateTask.js

--- React-JS/React-Hooks/demo-todo-list/src/components/CreateTask.js
+++ React-JS/React-Hooks/demo-todo-list/src/components/CreateTask.js
@@ -3,16 +3,16 @@ import { useState } from 'react';
 const CreateTask = ({ taskCreateHandler }) => {
     const [task, setTask] = useState('');
 
+    const onChange = (e) => {
+        setTask(e.target.value)
+    }
+
     const onSubmit = (e) => {
         e.preventDefault();
         taskCreateHandler(task);
         setTask('');
     }
 
-    const onChange = (e) => {
-
-        setTask(e.target.value)
-    }
     return (
         <form onSubmit={onSubmit}>
             <input type='text' placeholder='Water flowers' name='taskName' value={task} onChange={onChange} />
