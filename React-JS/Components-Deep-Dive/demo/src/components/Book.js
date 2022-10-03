b5c74baeb5dea0cd5336313322010742e83e5985
delbusque <del.busque@gmail.com>
2022-10-03 10:44:14 +0300

    Added useEffect on mounting and updating

--- React-JS/Components-Deep-Dive/demo/src/components/Book.js
+++ React-JS/Components-Deep-Dive/demo/src/components/Book.js
@@ -1,6 +1,30 @@
+import { click } from "@testing-library/user-event/dist/click.js";
+import { useState, useEffect } from "react"
+
 export const Book = (props) => {
+
+    const [marked, setMarked] = useState(false);
+
+    useEffect(() => {
+        console.log('Mounting');
+    }, [])
+
+    useEffect(() => {
+        console.log('Updating');
+    }, [marked])
+
+    const clickHandler = () => {
+        setMarked(oldStyle => !oldStyle);
+    }
+
+    let styles = {};
+
+    if (marked) {
+        styles.backgroundColor = 'pink';
+    }
+
     return (
-        <div>
+        <div onClick={clickHandler} style={styles}>
             <h2>{props.title}</h2>
             <h3>{props.author}</h3>
             <span>{props.year}, </span>
