ad00f462e95cd7ecf632a5f033f061c05ff0b109
delbusque <del.busque@gmail.com>
2022-10-27 17:02:27 +0300

    Added Add New user service

--- React-JS/Workshop-Components/demo-user-list/client/src/services/userService.js
+++ React-JS/Workshop-Components/demo-user-list/client/src/services/userService.js
@@ -1,8 +1,15 @@
 const baseUrl = 'http://localhost:3005/api/users';
 
-
 export const getAll = async () => {
     const responce = await fetch(baseUrl);
     const result = await responce.json();
     return result;
 }
+
+export const addUser = (userData) => fetch(baseUrl, {
+    method: 'POST',
+    headers: {
+        'content-type': 'application/json'
+    },
+    body: JSON.stringify(userData)
+}).then(res => res.json())
