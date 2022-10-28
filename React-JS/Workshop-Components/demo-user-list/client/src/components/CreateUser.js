6156882d4a52d67dc1bad41efec374d4d79d287d
delbusque <del.busque@gmail.com>
2022-10-28 18:03:15 +0300

    Update CreateUser.js

--- React-JS/Workshop-Components/demo-user-list/client/src/components/CreateUser.js
+++ React-JS/Workshop-Components/demo-user-list/client/src/components/CreateUser.js
@@ -27,7 +27,7 @@ export const CreateUser = ({ newUserHandler, users, setUsers }) => {
             <div className="modal">
                 <div className="user-container">
                     <header className="headers">
-                        <h2>Edit User/Add User</h2>
+                        <h2>Add User</h2>
                         <button className="btn close" onClick={newUserHandler}>
                             <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="xmark"
                                 className="svg-inline--fa fa-xmark" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
