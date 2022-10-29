274f033277d5504cc4a6fe076ce7ac824a3d931b
delbusque <del.busque@gmail.com>
2022-10-29 12:29:23 +0300

    Added edit User component

--- React-JS/Workshop-Components/demo-user-list/client/src/App.js
+++ React-JS/Workshop-Components/demo-user-list/client/src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import { useState, useEffect } from 'react'
 import * as userService from './services/userService.js'
 
-
 import { Header } from './components/Header.js';
 import { Footer } from './components/Footer.js';
 import { Search } from './components/Search.js';
@@ -12,6 +11,7 @@ import { CreateUser } from './components/CreateUser.js';
 import { AddNewUser } from './components/AddNewUser.js';
 import { UserInfo } from './components/UserInfo.js';
 import { DeleteUser } from './components/DeleteUser.js';
+import { EditUser } from './components/EditUser.js';
 
 function App() {
 
@@ -27,6 +27,7 @@ function App() {
     const [isAddNewUser, setIsAddNewUser] = useState(false);
     const [selectedUser, setSelectedUser] = useState(null);
     const [deletedUser, setDeletedUser] = useState(null);
+    const [editedUser, setEditedUser] = useState(null);
 
     const newUserHandler = () => {
         setIsAddNewUser(oldState => !oldState);
@@ -39,11 +40,15 @@ function App() {
         if (e.title == 'Delete') {
             setDeletedUser(user);
         }
+        if (e.title == 'Edit') {
+            setEditedUser(user);
+        }
     }
 
-    const closeHandler = (user) => {
+    const closeHandler = () => {
         setSelectedUser(null);
         setDeletedUser(null);
+        setEditedUser(null);
     }
 
     return (
@@ -70,6 +75,10 @@ function App() {
                     {deletedUser &&
                         users.map(user => deletedUser._id == user._id && <DeleteUser users={users} setUsers={setUsers} key={user._id} deletedUser={deletedUser} closeHandler={closeHandler} />)
                     }
+
+                    {editedUser && users.map(user => editedUser._id == user._id && <EditUser key={user._id}
+                        editedUser={editedUser} closeHandler={closeHandler} users={users} setUsers={setUsers}
+                        setEditedUser={setEditedUser} />)}
 
                     <Pagination />
                 </section>
