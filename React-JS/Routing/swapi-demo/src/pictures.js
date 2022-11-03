d5ed80ca994de9e0dc919f3d0ffa21ed7e6b9422
delbusque <del.busque@gmail.com>
2022-11-03 13:15:36 +0200

    Create pictures.js

---
+++ React-JS/Routing/swapi-demo/src/pictures.js
@@ -0,0 +1,32 @@
+export const pictures = [
+    {
+        episode_id: 4,
+        imgUrl: 'https://lumiere-a.akamaihd.net/v1/images/Star-Wars-New-Hope-IV-Poster_c217085b.jpeg?region=40%2C219%2C586%2C293'
+    },
+    {
+        episode_id: 5,
+        imgUrl: 'https://lumiere-a.akamaihd.net/v1/images/Star-Wars-Empire-Strikes-Back-V-Poster_878f7fce.jpeg?region=25%2C299%2C612%2C306'
+    },
+    {
+        episode_id: 6,
+        imgUrl: 'https://lumiere-a.akamaihd.net/v1/images/Star-Wars-Return-Jedi-VI-Poster_a10501d2.jpeg?region=9%2C210%2C624%2C312'
+    },
+    {
+        episode_id: 1,
+        imgUrl: 'https://lumiere-a.akamaihd.net/v1/images/Star-Wars-Phantom-Menace-I-Poster_f5832812.jpeg?region=0%2C250%2C678%2C340'
+    },
+    {
+        episode_id: 2,
+        imgUrl: 'https://lumiere-a.akamaihd.net/v1/images/Star-Wars-Attack-Clones-II-Poster_53baa2e7.jpeg?region=0%2C188%2C678%2C340'
+    },
+    {
+        episode_id: 3,
+        imgUrl: 'https://lumiere-a.akamaihd.net/v1/images/Star-Wars-Revenge-Sith-III-Poster_646108ce.jpeg?region=0%2C356%2C746%2C374'
+    },
+    {
+        episode_id: 7,
+        imgUrl: 'https://lumiere-a.akamaihd.net/v1/images/avco_payoff_1-sht_v7_lg_32e68793.jpeg?region=118%2C252%2C1384%2C696&width=768'
+    },
+
+]
+
