2c1c47b51d3b891fec0ee03f68411d6ff0456937
delbusque <del.busque@gmail.com>
2022-11-05 14:43:28 +0200

    Update Films.js

--- React-JS/Routing/swapi-demo/src/components/films/Films.js
+++ React-JS/Routing/swapi-demo/src/components/films/Films.js
@@ -1,12 +1,13 @@
-import './Films.modules.css'
+import styles from './Films.module.css'
+
 import { Link } from "react-router-dom";
 
 export default function Films({ films }) {
 
     return (
-        <ul className='film-ul'>
+        <ul className={styles['film-ul']}>
             {films.map(film =>
-                <Link to={`/films/${film.episode_id}`} className='film-link' key={film.episode_id} >
+                <Link to={`/films/${film.episode_id}`} className={styles['film-link']} key={film.episode_id} >
                     {film.title}
                 </Link>)
             }
