c186b26ddd8290537813851c5bd0033ef485fadd
delbusque <del.busque@gmail.com>
2022-11-08 17:26:53 +0200

    Update Home.js

--- React-JS/Routing/swapi-demo/src/components/Home.js
+++ React-JS/Routing/swapi-demo/src/components/Home.js
@@ -1,10 +1,7 @@
-import './Home.module.css'
-
 export default function Home() {
 
     return (
-        <div className='home-wrapper'>
+        <div>
             <img className='home-img' src="https://lumiere-a.akamaihd.net/v1/images/solo-a-star-wars-story-theatrical-poster-2_f4af9297.jpeg?region=0%2C397%2C1298%2C646&width=768" alt="" />
-
         </div>)
 }
