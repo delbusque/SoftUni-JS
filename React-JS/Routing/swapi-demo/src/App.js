8c82c9658f1c6ef0460e6e6bd6fc757f1a80201f
delbusque <del.busque@gmail.com>
2022-11-08 17:27:35 +0200

    Added User Context along with login

--- React-JS/Routing/swapi-demo/src/App.js
+++ React-JS/Routing/swapi-demo/src/App.js
@@ -1,8 +1,14 @@
+// import { useFetch } from './hooks/useFetch.js'
+// const baseUrl = 'https://swapi.py4e.com/api/films';
+// let filmsData = useFetch(baseUrl);
+
 import { useState, useEffect } from 'react'
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import './App.css';
 
-import { FilmsContext } from './components/films/FilmContext.js';
+import { FilmsContext } from './contexts/FilmContext.js';
+import { PlanetsContext } from './contexts/PlanetsContext.js'
+import { UserContext } from './contexts/UserConext.js';
 
 import * as swapiService from './services/swapiService.js'
 
@@ -11,50 +17,64 @@ import Home from './components/Home.js';
 import Films from './components/films/Films.js';
 import People from './components/People.js';
 import Planets from './components/Planets.js';
-import Species from './components/Species.js';
-import Vehicles from './components/Vehicles.js';
+import Login from './components/Login.js';
+import Register from './components/Register.js';
 import Starships from './components/Starships.js';
 import { FilmCard } from './components/films/FilmCard.js';
 
-import { useFetch } from './components/hooks/useFetch.js'
-
-const baseUrl = 'https://swapi.py4e.com/api/films';
+import * as authService from './services/authService.js'
 
 function App() {
+  const navigate = useNavigate();
 
   const [api, setApi] = useState({});
+  const [films, setFilms] = useState([]);
+  const [planets, setPlanets] = useState([]);
+  const [user, setUser] = useState({});
+
   useEffect(() => {
     swapiService.getApi().then(data => setApi(data));
+    swapiService.getFilms().then(result => setFilms(result.results));
+    swapiService.getPlanets().then(result => setPlanets(result.results));
   }, [])
 
-  const [films, setFilms] = useState([]);
-  let filmsData = useFetch(baseUrl);
-  console.log(filmsData);
-
-  useEffect(() => {
-    swapiService.getFilms().then(result => setFilms(result.results));
-  }, []);
+  const submitHandler = async (values, e) => {
+    e.preventDefault();
+    const result = await authService.login(values);
+    if (result.email) {
+      setUser(result)
+      console.log(user);
+      return navigate('/')
+    } else {
+      return navigate('/starships')
+    }
+  }
 
   return (
-    <div className="App ">
-      <div className="App-header">
-        <FilmsContext.Provider value={films}>
+    <UserContext.Provider value={{ submitHandler, user }}>
+      <div className="App ">
+        <div className="App-header">
+
           <Navigation api={api} />
+          <PlanetsContext.Provider value={planets}>
+            <FilmsContext.Provider value={films}>
+              <Routes>
+                <Route path='/' element=<Home /> />
+                <Route path='/films' element=<Films /> />
+                <Route path='/people' element=<People /> />
+                <Route path='/planets' element=<Planets /> />
+                <Route path='/login' element=<Login /> />
+                <Route path='/register' element=<Register /> />
+                <Route path='/starships' element=<Starships /> />
 
-          <Routes>
-            <Route path='/' element=<Home /> />
-            <Route path='/films' element=<Films /> />
-            <Route path='/people' element=<People /> />
-            <Route path='/planets' element=<Planets /> />
-            <Route path='/species' element=<Species /> />
-            <Route path='/vehicles' element=<Vehicles /> />
-            <Route path='/starships' element=<Starships /> />
+                <Route path='/films/:filmId/*' element=<FilmCard /> />
+              </Routes>
+            </FilmsContext.Provider>
+          </PlanetsContext.Provider>
 
-            <Route path='/films/:filmId/*' element=<FilmCard /> />
-          </Routes>
-        </FilmsContext.Provider>
+        </div>
       </div>
-    </div>
+    </UserContext.Provider>
   );
 }
 
