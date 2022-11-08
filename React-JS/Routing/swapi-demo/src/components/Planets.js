ddad3caa422c93c7e54b9e63416a52d9f4faea7c
delbusque <del.busque@gmail.com>
2022-11-08 17:19:52 +0200

    Added Planets Context

--- React-JS/Routing/swapi-demo/src/components/Planets.js
+++ React-JS/Routing/swapi-demo/src/components/Planets.js
@@ -1,5 +1,21 @@
+import styles from '../components/films/Films.module.css'
+
+import { Link } from "react-router-dom";
+import { useContext } from 'react';
+import { PlanetsContext } from '../contexts/PlanetsContext.js';
+
+
 export default function Planets() {
+
+    const planets = useContext(PlanetsContext);
+    console.log(planets);
     return (
-        <header></header>
+        <ul className={styles['film-ul']}>
+            {planets.map(planet =>
+                <Link to={`/planets/${planet.name}`} className={styles['planet-link']} key={planet.name} >
+                    {planet.name}
+                </Link>)
+            }
+        </ul >
     )
 }
