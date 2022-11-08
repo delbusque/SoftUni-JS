80a12cf0845bf1eaf89b5dd7a2c1128dee1139ab
delbusque <del.busque@gmail.com>
2022-11-08 17:26:07 +0200

    Navigation refactured

--- React-JS/Routing/swapi-demo/src/components/navigation/Navigation.js
+++ React-JS/Routing/swapi-demo/src/components/navigation/Navigation.js
@@ -1,14 +1,28 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import './Navigation.modules.css'
-import { NavLin } from './NavLin.js'
+import uniqid from 'uniqid';
 
 export default function Navigation({ api }) {
+
     const apis = Object.keys(api);
+
     return (
         <ul className='nav-wrapper'>
-            <Link className='nav' to='/'><img className='nav-img' src='https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/SW_opening_crawl_logo.svg/1200px-SW_opening_crawl_logo.svg.png' /></Link>
+            <Link className='nav' to='/'><img className='nav-img' src='https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/SW_opening_crawl_logo.svg/1200px-SW_opening_crawl_logo.svg.png' alt='home' /></Link>
 
-            {apis.map(a => <NavLin api={a} key={a} className='nav' to={`/${a}`}>{a}</NavLin>)}
+            <NavLink key={uniqid()} className='nav' to={`/films`}
+                style={({ isActive }) => ({ background: isActive ? '#e3c318' : undefined })}>Films</NavLink>
+            <NavLink key={uniqid()} className='nav' to={`/planets`}
+                style={({ isActive }) => ({ background: isActive ? 'aquamarine' : undefined })}>Planets</NavLink>
+            <NavLink key={uniqid()} className='nav' to={`/people`}
+                style={({ isActive }) => ({ background: isActive ? 'palegoldenrod' : undefined })}>People</NavLink>
+            <NavLink key={uniqid()} className='nav' to={`/starships`}
+                style={({ isActive }) => ({ background: isActive ? 'lightgrey' : undefined })}>Starships</NavLink>
+
+            <div className='nav-auth'>
+                <NavLink style={({ isActive }) => ({ background: isActive ? 'antiquewhite' : undefined })} className='auth' to='/login'>Login</NavLink>
+                <NavLink style={({ isActive }) => ({ background: isActive ? 'antiquewhite' : undefined })} className='auth' to='/register'>Register</NavLink>
+            </div>
         </ul>
 
     )
