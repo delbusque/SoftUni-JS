8e904d01c8370059e70a80c1f7d7460707d3f0a9
delbusque <del.busque@gmail.com>
2022-11-09 09:46:15 +0200

    People and Starships component used for redirects

--- React-JS/Routing/swapi-demo/src/components/People.js
+++ React-JS/Routing/swapi-demo/src/components/People.js
@@ -1,6 +1,6 @@
 export default function People() {
 
     return (
-        <header></header>
+        <h4 className="redirect">You`ve been successfully signed out !</h4>
     )
 }
--- React-JS/Routing/swapi-demo/src/components/Starships.js
+++ React-JS/Routing/swapi-demo/src/components/Starships.js
@@ -1,5 +1,5 @@
 export default function Starships() {
     return (
-        <header></header>
+        <h4 className="redirect">You've been redirected with error 403 !</h4>
     )
 }
