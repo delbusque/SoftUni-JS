0044649f88811ebff6004b15498f540c9fc0177b
delbusque <del.busque@gmail.com>
2022-11-09 10:08:31 +0200

    Added User Context to People and Starships for conditional rendering

--- React-JS/Routing/swapi-demo/src/components/People.js
+++ React-JS/Routing/swapi-demo/src/components/People.js
@@ -1,6 +1,12 @@
-export default function People() {
+import { useContext } from "react"
+import { UserContext } from "../contexts/UserConext.js"
 
+export default function People() {
+    const { user } = useContext(UserContext);
     return (
-        <h4 className="redirect">You`ve been successfully signed out !</h4>
+        <>
+            {!user.email && <h4 className="redirect">You`ve been successfully signed out !</h4>}
+        </>
+
     )
 }
--- React-JS/Routing/swapi-demo/src/components/Starships.js
+++ React-JS/Routing/swapi-demo/src/components/Starships.js
@@ -1,5 +1,12 @@
+import { useContext } from "react"
+import { UserContext } from "../contexts/UserConext.js"
+
 export default function Starships() {
+
+    const { user } = useContext(UserContext);
     return (
-        <h4 className="redirect">You've been redirected with error 403 !</h4>
+        <>
+            {!user.email && <h4 className="redirect">You've been redirected with error 403 !</h4>}
+        </>
     )
 }
