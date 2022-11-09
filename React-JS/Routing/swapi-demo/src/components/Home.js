62fa88778bede094eb5cc9b416f19a1f2bd73d67
delbusque <del.busque@gmail.com>
2022-11-09 09:49:12 +0200

    Added styles

--- React-JS/Routing/swapi-demo/src/components/Home.js
+++ React-JS/Routing/swapi-demo/src/components/Home.js
@@ -1,7 +1,9 @@
+import styles from './Home.module.css'
+
 export default function Home() {
 
     return (
         <div>
-            <img className='home-img' src="https://lumiere-a.akamaihd.net/v1/images/solo-a-star-wars-story-theatrical-poster-2_f4af9297.jpeg?region=0%2C397%2C1298%2C646&width=768" alt="" />
+            <img className={styles['home-img']} src="https://lumiere-a.akamaihd.net/v1/images/solo-a-star-wars-story-theatrical-poster-2_f4af9297.jpeg?region=0%2C397%2C1298%2C646&width=768" alt="" />
         </div>)
 }
